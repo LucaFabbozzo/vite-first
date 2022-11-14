6d5d4341c4eee8ccbce7fa5548711c11897e50bc
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-14 10:12:39 -0500

    [add] my first app with Vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+      title: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="container">
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+
+<style>
+  h2 {
+    color: #272727;
+    text-transform: uppercase;
+  }
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 100px auto;
+    width: 50%;
+    height: 300px;
+    text-align: center;
+    background-color: #f2f2f2;
+    border: 1px solid black;
+  }
+</style>
